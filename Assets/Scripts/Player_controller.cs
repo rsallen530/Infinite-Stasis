18db7f543a25e624ca49ab3a6359697a42916588
rsallen530 <148916403+rsallen530@users.noreply.github.com>
2023-11-16 14:44:09 -0800

    First Commit
    Made a player controller, basic testing levels and created an enemy script

---
+++ Assets/Scripts/Player_controller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed2 = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        transform.Rotate(0, speed2, 0);
+
+    }
+}
