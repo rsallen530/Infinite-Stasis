bc29e01f3ec0626ab3e24f3d0054515a22434524
rpursell <148917102+rpursell@users.noreply.github.com>
2023-11-16 15:37:21 -0800

    FPS Controller
    Makes the camera first-person and adds camera movement.

--- Assets/Scripts/Player_controller.cs
+++ Assets/Scripts/Player_controller.cs
@@ -4,35 +4,43 @@ using UnityEngine;
 
 public class Player_controller : MonoBehaviour
 {
+    public GameObject camera;
     public float speed = 10f;
+    public float mouseSpeed = 10f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float speed2 = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            transform.position += -transform.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            transform.position += transform.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * speed * Time.deltaTime;
+            transform.position += transform.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.back * speed * Time.deltaTime;
+            transform.position += -transform.forward * speed * Time.deltaTime;
         }
-        transform.Rotate(0, speed2, 0);
+        yaw += mouseSpeed * Input.GetAxis("Mouse X");
+        pitch -= mouseSpeed * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+        camera.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
     }
 }
