1216bc4d8b7792192ea359c59c2256749fa96f65
rsallen530 <Rsallen@mail.csuchico.edu>
2023-11-20 19:51:27 -0800

    made the enemy follow the player
    I added to the enemy script making it so now the enemy follows the player. I also made the plane bigger and made a materials folder and gave the player, enemy and plane a material.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 20f;
+    public float midDist = 1f;
+    public Transform target; 
+
     // Start is called before the first frame update
     void Start()
-    {
+    { if (target == null)
+        {
+            if (GameObject.FindWithTag("Player") != null)
+            {
+                target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+            }
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+            return;
+        //face the target 
+        transform.LookAt(target);
+        float distance = Vector3.Distance(transform.position, target.position);
+        if (distance > midDist)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+        }
+       
+   
+    
+    
+    
     }
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+
+
 }
